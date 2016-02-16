d370ded2ce0d626920ad55253341d029af89cedd
Yegor <Babosiuk@gmail.com>
2016-02-17 00:31:10 +0200

    Event List style

--- mf-frontend/events.js
+++ mf-frontend/events.js
@@ -7,6 +7,13 @@ var MOKED_DATA = [
     {userName:'BordyArt(Artyom)', title: 'My Happy Birdthday', description: 'Lets pray in church together', partyType: 'Party', date: 'Today 22:00', favorite: 'true', userImage: {thumbnail:'http://i.imgur.com/UePbdph.jpg'}},
     {userName:'Yegor Babosiuk', title: 'Party na hate', description: 'I want some crazy party with girls', partyType: 'Party', date: 'Tommorow 20:00', favorite: 'true', userImage: {thumbnail:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNaRRe4I7cwuC12R0zkUNP71rpA7vqwVvR5yDkXWyoA_1d8tyd9A'}},
     {userName:'Kolya', title: 'Play games with friendsy', description: 'Lets play mortal combat', partyType: 'Games', date: '25.09.2016', favorite: 'true', userImage: {thumbnail:'http://ndl.mgccw.com/mu3/app/20150810/04/1439162148803/icon/icon_xl.png'}},
+    {userName:'BordyArt(Artyom)', title: 'My Happy Birdthday', description: 'Lets pray in church together', partyType: 'Party', date: 'Today 22:00', favorite: 'true', userImage: {thumbnail:'http://i.imgur.com/UePbdph.jpg'}},
+    {userName:'Yegor Babosiuk', title: 'Party na hate', description: 'I want some crazy party with girls', partyType: 'Party', date: 'Tommorow 20:00', favorite: 'true', userImage: {thumbnail:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNaRRe4I7cwuC12R0zkUNP71rpA7vqwVvR5yDkXWyoA_1d8tyd9A'}},
+    {userName:'Kolya', title: 'Play games with friendsy', description: 'Lets play mortal combat', partyType: 'Games', date: '25.09.2016', favorite: 'true', userImage: {thumbnail:'http://ndl.mgccw.com/mu3/app/20150810/04/1439162148803/icon/icon_xl.png'}},
+    {userName:'BordyArt(Artyom)', title: 'My Happy Birdthday', description: 'Lets pray in church together', partyType: 'Party', date: 'Today 22:00', favorite: 'true', userImage: {thumbnail:'http://i.imgur.com/UePbdph.jpg'}},
+    {userName:'Yegor Babosiuk', title: 'Party na hate', description: 'I want some crazy party with girls', partyType: 'Party', date: 'Tommorow 20:00', favorite: 'true', userImage: {thumbnail:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNaRRe4I7cwuC12R0zkUNP71rpA7vqwVvR5yDkXWyoA_1d8tyd9A'}},
+    {userName:'Kolya', title: 'Play games with friendsy', description: 'Lets play mortal combat', partyType: 'Games', date: '25.09.2016', favorite: 'true', userImage: {thumbnail:'http://ndl.mgccw.com/mu3/app/20150810/04/1439162148803/icon/icon_xl.png'}},
+
 ];
 var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
@@ -68,27 +75,37 @@ var Events = new React.createClass({
                     source={{uri: event.userImage.thumbnail}}
                     style={styles.thumbnail}
                 />
-                <View style={styles.rightContainer}>
-                    <Text>
+                <View style={styles.leftContainer}>
+                    <Text style={styles.userName}>
                         {event.userName}
                     </Text>
                     <Text style={styles.title}>
                         {event.title}
                     </Text>
-                    <Text>
+                    <Text style={styles.description}>
                         {event.description}
                     </Text>
-                    <Text style={styles.date}>
-                        {event.date}
-                    </Text>
-                    <Text>
+
+                    <Text style={styles.partyType}>
                         {event.partyType}
                     </Text>
                 </View>
-                <ProgressBarAndroid style={styles.spinner}
-                                    size="Small"
+                <View style={styles.rightContainer}>
+                    <Text style={styles.date}>
+                        {event.date}
+                    </Text>
+                    <View style={styles.iconRow}>
+                        <Image
+                            source={{uri: event.userImage.thumbnail}}
+                            style={styles.icon}
+                        />
+                        <Image
+                            source={{uri: event.userImage.thumbnail}}
+                            style={styles.icon}
+                        />
+                    </View>
+                </View>
 
-                />
             </View>
         )
     },
@@ -109,26 +126,71 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#F5FCFF'
+        backgroundColor: '#F5FCFF',
+        borderWidth: 1,
+        borderColor: '#d4d4d4'
     },
     spinner: {
         opacity: 1
     },
     thumbnail: {
-        width: 53,
-        height: 81
+        width: 50,
+        height: 50,
+        borderRadius: 30
     },
-    rightContainer: {
-        flex: 1
+    leftContainer: {
+        flex: 4
+    },
+    userName: {
+        textAlign: 'left',
+        marginTop: 10,
+        marginLeft: 10,
+        fontWeight: 'bold',
+        fontSize: 14,
+        color: "#424242"
     },
     title: {
-        fontSize: 20,
-        marginBottom: 8,
-        textAlign: 'center'
+        fontSize: 13,
+        textAlign: 'left',
+        marginLeft: 10,
+        marginTop: 5,
+        color: "#464646"
     },
     date: {
-        textAlign: 'center'
+        fontSize: 9,
+        textAlignVertical : 'top',
+        marginRight : 10
+    },
+    description: {
+        fontSize: 11,
+        textAlign: 'left',
+        marginLeft: 10,
+        marginTop: 5,
+        color: "#464646"
+    },
+    partyType: {
+        fontStyle: 'italic',
+        fontSize: 11,
+        textAlign: 'left',
+        marginLeft: 10,
+        marginTop: 5,
+        marginBottom: 5
+    },
+    icon : {
+        width: 15,
+        height: 15,
+        borderRadius: 30,
+        marginTop: 10,
+        marginLeft: 10,
+    },
+    iconRow : {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    rightContainer : {
+        flex: 1
     }
+
 });
 
 module.exports = Events
